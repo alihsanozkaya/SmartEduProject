d10a0876128bc71e4ab77699981e9c56f8aa021a
Ali Ihsan Ozkaya <ihsanozkaya1903@gmail.com>
2024-07-23 14:38:45 +0300

    Lesson 26

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -25,5 +25,6 @@ router
 router.route("/login").post(authController.loginUser);
 router.route("/logout").get(authController.logoutUser);
 router.route("/dashboard").get(authMiddleware, authController.getDashboardPage);
+router.route("/:id").delete(authController.deleteUser);
 
 module.exports = router;
